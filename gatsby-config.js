83de0330c9b4152048a1a42c9ae828c34b465bde
Manoj M S <59591622+Manoj-M-S@users.noreply.github.com>
2020-03-19 11:47:38 +0530

    First Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Manoj M S`,
+      summary: `who lives and works in Bangalore building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://github.com/Manoj-M-S`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `manojmakarasu`,
     },
   },
   plugins: [
